0a2a4ed581d774a41f7fb22cf09048208bd5d127
rabegy <ratawfik@gmail.com>
2023-02-03 12:01:36 +0000

    Blackbox add two digits

---
+++ Warm.py
@@ -0,0 +1,19 @@
+print ("hello")
+print ("trying block box")
+
+#How to add two digits?
+def solveMeFirst(a,b):
+
+   return a+b
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+res = solveMeFirst(num1,num2)
+print(res)
+
+
+
+
+#Source: https://stackoverflow.com/questions/60589052
+
+solveMeFirst(10,10)
