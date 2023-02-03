9f79d11536570a95719bfa07524097773cd650e9
rabegy <ratawfik@gmail.com>
2023-02-03 15:05:44 +0000

    Index added

--- api.py
+++ api.py
@@ -3,7 +3,10 @@ from flask import Flask, render_template
 app = Flask (__name__)
 
 @app.route('/')
+@app.route('/index')
 def index():
-        return 'Hello, World!'
+    name = 'Rosalia'
+    return render_template('index.html', title='Welcome', username=name)
+
 
 app.run()
